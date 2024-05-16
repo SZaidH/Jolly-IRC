5e98d16670c39c02f05726e57b982b6ff4f57092
Zaid Hasan <szh619@gmail.com>
2024-05-16 14:01:32 +0300

    Updated Dicebear API URL

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Avatar } from '@material-ui/core'
+import React from "react";
+import { Avatar } from "@material-ui/core";
 
 function MemberList({ members }) {
-
   const memberObj = {};
   // Looping through the members state to push the unique members (eliminating redundancy) to the member object
-  members.forEach(element => {
+  members.forEach((element) => {
     if (!memberObj[element.member]) {
       memberObj[element.member] = element.member;
     }
@@ -15,19 +14,21 @@ function MemberList({ members }) {
     <li key={i}>
       <div className="member-item">
         {/* Passing the member key to the Dicebear API URL to fetch initials for the User Avatar */}
-        <Avatar alt="Member Picture" className="member-avatar" src={`https://avatars.dicebear.com/api/initials/${memberObj[memberKey]}.svg`} />
+        <Avatar
+          alt="Member Picture"
+          className="member-avatar"
+          src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${memberObj[memberKey]}`}
+        />
         <p>{memberObj[memberKey]}</p>
       </div>
     </li>
-  ))
+  ));
 
   return (
     <div className="member-list">
-      <ul>
-        {!memberObj ? null : memberList}
-      </ul>
+      <ul>{!memberObj ? null : memberList}</ul>
     </div>
-  )
+  );
 }
 
-export default MemberList
+export default MemberList;
